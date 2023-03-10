0905720b151e9585eeafd6204c6bb23a417f17c3
barman <attapon.peungsook@scbtechx.io>
2023-03-10 14:20:56 +0700

    add component mission

---
+++ lib/components/mission.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Mission extends StatelessWidget {
+  const Mission({Key? key, required this.title, required this.color})
+      : super(key: key);
+
+  final String title;
+  final int color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            color: Colors.amber[color]),
+        margin: const EdgeInsets.all(8),
+        height: 100,
+        child: Center(child: Text('Entry $title')));
+  }
+}
--- lib/page/dashboard.dart
+++ lib/page/dashboard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/mission.dart';
+
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
 
@@ -10,14 +12,24 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
+    debugPrint('Dashboard run');
+    final List<String> entries = <String>['A', 'B', 'C', 'D', 'E', 'F'];
+    final List<int> colorCodes = <int>[600, 500, 400, 300, 200, 100];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
-      body: const Center(
-        child: Text(
-          'Dashboard Screen',
-          style: TextStyle(fontSize: 40),
+      body: Center(
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: entries.length,
+          itemBuilder: (context, index) {
+            return Mission(
+              title: entries[index],
+              color: colorCodes[index],
+            );
+          },
         ),
       ),
     );
