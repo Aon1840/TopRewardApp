e641bf9e74eab40227437b55599ceff5b4690d55
barman <attapon.peungsook@scbtechx.io>
2023-03-10 17:10:13 +0700

    add progress bar

--- lib/components/mission.dart
+++ lib/components/mission.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intervalprogressbar/intervalprogressbar.dart';
 
 class Mission extends StatelessWidget {
   const Mission({Key? key, required this.title, required this.color})
@@ -15,6 +16,43 @@ class Mission extends StatelessWidget {
             color: Colors.amber[color]),
         margin: const EdgeInsets.all(8),
         height: 100,
-        child: Center(child: Text('Entry $title')));
+        child: Row(
+          children: [
+            Container(
+                height: 80,
+                width: 80,
+                margin: const EdgeInsets.only(left: 10),
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.black54)),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, top: 10),
+                  child: Text('Entry $title'),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10, top: 10),
+                  child: IntervalProgressBar(
+                      direction: IntervalProgressDirection.horizontal,
+                      max: 3,
+                      progress: 1,
+                      intervalSize: 3,
+                      size: Size(200, 10),
+                      highlightColor: Colors.black,
+                      defaultColor: Colors.grey,
+                      intervalColor: Colors.pink,
+                      intervalHighlightColor: Colors.transparent,
+                      radius: 5),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, top: 10),
+                  child: Text('Entry $title'),
+                ),
+              ],
+            )
+          ],
+        ));
+    // child: Center(child: Text('Entry $title')));
   }
 }
